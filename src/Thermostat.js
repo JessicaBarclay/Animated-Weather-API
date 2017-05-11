f211e4938711ab17e6adfccb2619900cb90c3a06
Alex Chalk <alexchalk17@gmail.com>
2017-05-11 07:30:16 -0400

    Thermostat: Implement min temperature of 10

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,11 +2,20 @@
 
 var Thermostat = function() {
   this.temperature = 20;
+  this.MIN_TEMPERATURE_LIMIT = 10;
 };
 Thermostat.prototype.getTemperature = function() { return this.temperature; };
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MIN_TEMPERATURE_LIMIT;
+};
+
 Thermostat.prototype.up = function() {
   this.temperature += 1;
 };
 Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
   this.temperature -= 1;
 };
