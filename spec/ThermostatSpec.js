9b952913aeef733ba99b1867786c2d17151c8df9
Alex Chalk <alexchalk17@gmail.com>
2017-05-11 07:10:50 -0400

    Thermostat: Add up function to increase temperature by 1

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,20 +1,22 @@
-'use strict';
+(function() {
+  'use strict';
 
-describe('Thermostat', function() {
+  describe('Thermostat', function() {
 
-  var thermostat;
+    var thermostat;
 
-  beforeEach(function() {
-    thermostat= new Thermostat();
+    beforeEach(function() {
+      thermostat= new Thermostat();
+    });
+
+    it('has a default starting temp of 20', function() {
+      expect(thermostat.getTemperature()).toBe(20);
+    });
+
+    it('can increase the temperature with an up function', function() {
+      thermostat.up();
+      expect(thermostat.getTemperature()).toBe(21);
+    });
+
   });
-
-  it('has a default starting temp of 20', function() {
-    expect(thermostat.getTemperature()).toBe(20);
-  });
-
-  it('can increase the temperature with an up function', function() {
-    thermostat.up()
-    expect(thermostat.getTemperature()).toBe(21);
-  });
-
-});
+}());
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,3 +4,6 @@ var Thermostat = function() {
   this.temperature = 20;
 };
 Thermostat.prototype.getTemperature = function() { return this.temperature; };
+Thermostat.prototype.up = function() {
+  this.temperature += 1;
+};
