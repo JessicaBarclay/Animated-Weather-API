87730406801cf17bcbc6df71cce59d229a5279c6
milesillsley <milesillsley@hotmail.co.uk>
2017-05-11 12:05:50 +0100

    failing test for up function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,13 +5,16 @@ describe('Thermostat', function() {
   var thermostat;
 
   beforeEach(function() {
-
     thermostat= new Thermostat();
-
   });
 
   it('has a default starting temp of 20', function() {
     expect(thermostat.getTemperature()).toBe(20);
+  });
+
+  it('can increase the temperature with an up function', function() {
+    thermostat.up()
+    expect(thermostat.getTemperature()).toBe(21);
   });
 
 });
