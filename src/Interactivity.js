fa833c19a4b3dbc1a569430560821ec7dd132fb7
milesillsley <milesillsley@hotmail.co.uk>
2017-05-11 17:53:11 +0100

    Thermostat: add interactivity to html page

--- src/Interactivity.js
+++ src/Interactivity.js
@@ -1,12 +1,30 @@
 (function() {
   'use strict';
   $(document).ready(function() {
-    $("a").click(function(event) {
-      alert("Thanks for visiting!");
+    var thermostat = new Thermostat();
+
+    $("a#up").click(function(event) {
+      thermostat.up();
+      $("#temperature_display").text(thermostat.getTemperature());
+      $("#power_usage").text(thermostat.energyUsage());
     });
-    $("a").click(function(event) {
-      $(this).hide("slow");
+    $("a#down").click(function(event) {
+      thermostat.down();
+      $("#temperature_display").text(thermostat.getTemperature());
+      $("#power_usage").text(thermostat.energyUsage());
     });
-      
+    $("a#reset").click(function(event) {
+      thermostat.reset();
+      $("#temperature_display").text(thermostat.getTemperature());
+      $("#power_usage").text(thermostat.energyUsage());
+    });
+    $("a#power_saving_toggle").click(function(event) {
+      thermostat.togglePowerSaving();
+      if (thermostat.isInPowerSaving()) {
+        $("#power_saving_toggle").text("On");
+      } else {
+        $("#power_saving_toggle").text("Off");
+      }
+    });
   });
 }());
