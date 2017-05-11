150e96f177fd614c92e24f8ce2326d0926a3269a
milesillsley <milesillsley@hotmail.co.uk>
2017-05-11 12:40:42 +0100

    Thermostat: Implement maximum temperature of 25 degrees

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,6 +3,7 @@
 var Thermostat = function() {
   this.temperature = 20;
   this.MIN_TEMPERATURE_LIMIT = 10;
+  this.MAX_TEMPERATURE_LIMIT = 25;
 };
 Thermostat.prototype.getTemperature = function() { return this.temperature; };
 
@@ -10,7 +11,14 @@ Thermostat.prototype.isMinimumTemperature = function() {
   return this.temperature === this.MIN_TEMPERATURE_LIMIT;
 };
 
+Thermostat.prototype.isMaximumTemperature = function() {
+  return this.temperature === this.MAX_TEMPERATURE_LIMIT;
+};
+
 Thermostat.prototype.up = function() {
+  if (this.isMaximumTemperature()) {
+    return;
+  }
   this.temperature += 1;
 };
 Thermostat.prototype.down = function() {
