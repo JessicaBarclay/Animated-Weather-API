ed2f3e3bebb5692113c701bebf0752b3c9b5a186
Alex Chalk <alexchalk17@gmail.com>
2017-05-11 07:55:48 -0400

    Thermostat: Can toggle power-saving mode
    When power-saving mode is off, max temp limit is 32 degrees

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,16 +3,34 @@
 var Thermostat = function() {
   this.temperature = 20;
   this.MIN_TEMPERATURE_LIMIT = 10;
-  this.MAX_TEMPERATURE_LIMIT = 25;
+  this.MAX_TEMP_POWER_SAVING_ON = 25;
+  this.MAX_TEMP_POWER_SAVING_OFF = 32;
+  this.powerSaving = true;
 };
 Thermostat.prototype.getTemperature = function() { return this.temperature; };
+
+Thermostat.prototype.isInPowerSaving = function() {
+  return this.powerSaving;
+};
 
 Thermostat.prototype.isMinimumTemperature = function() {
   return this.temperature === this.MIN_TEMPERATURE_LIMIT;
 };
 
 Thermostat.prototype.isMaximumTemperature = function() {
-  return this.temperature === this.MAX_TEMPERATURE_LIMIT;
+  if (this.isInPowerSaving()) {
+    return this.temperature === this.MAX_TEMP_POWER_SAVING_ON;
+  } else {
+    return this.temperature === this.MAX_TEMP_POWER_SAVING_OFF;
+  }
+};
+
+Thermostat.prototype.togglePowerSaving = function() {
+  if (this.isInPowerSaving() === true) {
+    this.powerSaving = false;
+  } else {
+    this.powerSaving = true;
+  }
 };
 
 Thermostat.prototype.up = function() {
@@ -21,6 +39,7 @@ Thermostat.prototype.up = function() {
   }
   this.temperature += 1;
 };
+
 Thermostat.prototype.down = function() {
   if (this.isMinimumTemperature()) {
     return;
